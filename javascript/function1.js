1a5487eae57ab888fefc7cafb83f721e7205c646
dsa00340 <dsa00340@naver.com>
2022-01-27 16:51:25 +0900

    20220127

--- javascript/function1.js
+++ javascript/function1.js
@@ -1,7 +1,16 @@
 //function.js
 function addNumber(first, last){//function정의
     var sum = first + last;
-    console.log(`두수의 합 : ${sum}`);
+    // console.log(`두수의 합 : ${sum}`);
+    return sum;
 }
 
-addNumber(10,20);//실행문
+var sum1 = addNumber(10,20);//실행문
+var sum2 = addNumber(100,200);
+
+var result = addNumber(sum1, sum2);
+console.log(`최종합계는 ${result}`);
+
+
+result = addNumber('Hello','World');
+console.log(result);
